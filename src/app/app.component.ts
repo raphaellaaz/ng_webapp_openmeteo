e23a85c95efe0931794e2ac96af524802c9afcb6
Raphael Laaz <raphaellaaz01@gmail.com>
2023-11-12 22:42:29 -0500

    Agrega componente 'app-com-selector' en la sección de selección

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {ComSelectorComponent} from "./com-selector/com-selector.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+     RouterOutlet,
+     MatSlideToggleModule,
+     ComSelectorComponent,
+    ],
+    
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather_wapp';
+
+  ngOnInit(): void {
+    console.log("Hola bro");
+  }
+  
 }
