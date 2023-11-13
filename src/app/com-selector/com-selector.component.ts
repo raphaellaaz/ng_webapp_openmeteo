5c3f0ea0997a1e9feb0c6be4adba57193c2b5aa6
Raphael Laaz <raphaellaaz01@gmail.com>
2023-11-12 22:46:21 -0500

    Funciones Creadas, importando servicios de APIs, e implementando ngModel para probar

--- src/app/com-selector/com-selector.component.ts
+++ src/app/com-selector/com-selector.component.ts
@@ -1,13 +1,65 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NomatimApiService } from '../services/nomatim-api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-com-selector',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    MatSlideToggleModule,
+    FormsModule,
+  ],
   templateUrl: './com-selector.component.html',
   styleUrl: './com-selector.component.scss'
 })
+
+
 export class ComSelectorComponent {
+  resultado: any;
+  coords: any;
+  inputaddress:string='';
+  
+  constructor(private nomatim: NomatimApiService){
+  }
+
+  getAddress():void{
+    this.nomatim.getDataNomatim(this.inputaddress).subscribe(
+      data=>{
+        this.resultado = data;
+        console.log(this.inputaddress);
+        console.log(this.resultado);
+      },
+      error =>{
+        console.error("Error en comoponente selector y api", error);
+      }
+    );
+  }
+
+
+  getLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          const location = `${latitude},${longitude}`;
+          this.coords=location;
+          console.log(location);
+        },
+        error => {
+          console.error('Error al obtener la ubicación:', error);
+        }
+      );
+    } else {
+      console.error('Geolocalización no está soportada por el navegador.');
+    }
+  }
+
+
+
+
 
 }
